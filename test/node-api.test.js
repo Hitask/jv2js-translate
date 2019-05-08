c64257b2da86da0ca6e3d4520bf156a6de306fc3
Aleksey Mironenko <mironenkoalexey@gmail.com>
2019-05-08 19:18:27 +0300

    Add test commands

---
+++ test/node-api.test.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const { compileLocales } = require('../index');
+
+const options = {
+    src: path.join(__dirname, 'src'),
+    dist: path.join(__dirname, 'dist'),
+}
+compileLocales(options)
+
