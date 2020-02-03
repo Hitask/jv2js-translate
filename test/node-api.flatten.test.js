1a15f41c5348bbccbde48c2912faed9a85819a24
Aleksey Mironenko <mironenkoalexey@gmail.com>
2020-02-03 18:13:15 +0200

    Prettify test files

--- test/node-api.flatten.test.js
+++ test/node-api.flatten.test.js
@@ -1,10 +1,10 @@
-const path = require('path')
+const path = require('path');
 const { compileLocales } = require('../index');
 
 const options = {
-    src: path.join(__dirname, 'src'),
-    dist: path.join(__dirname, 'dist'),
-    flatten: true,
-}
-compileLocales(options)
+	src: path.join(__dirname, 'src'),
+	dist: path.join(__dirname, 'dist'),
+	flatten: true,
+};
 
+compileLocales(options);
